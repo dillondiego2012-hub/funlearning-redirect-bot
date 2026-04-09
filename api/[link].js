aee7268740d304a3629ac49a7a4a722cb8a15f98
dillondiego2012-hub <dillondiego2012@gmail.com>
2026-04-09 07:38:53 -0500

    Update [link].js

--- api/[link].js
+++ api/[link].js
@@ -1,14 +1,22 @@
 export default function handler(req, res) {
   const links = {
-    link1: "https://sites.google.com/view/math-staar-review/home"
+    link1: [
+      "https://sites.google.com/view/math-staar-review/home",
+      "https://sites.google.com/view/math-staar-review/home",
+      "https://sites.google.com/view/math-staar-review/home"
+    ]
   };
 
   const { link } = req.query;
 
-  if (links[link]) {
-    res.writeHead(302, { Location: links[link] });
-    res.end();
-  } else {
-    res.status(404).send("Not found");
+  if (!links[link]) {
+    return res.status(404).send("Not found");
   }
+
+  const arr = links[link];
+
+  const index = Math.floor(Date.now() / (1000 * 60 * 60 * 24 * 3)) % arr.length;
+
+  res.writeHead(302, { Location: arr[index] });
+  res.end();
 }
