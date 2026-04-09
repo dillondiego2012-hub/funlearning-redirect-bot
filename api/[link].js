eed4a16538e20d004de56901947352c7bf68d0d7
dillondiego2012-hub <dillondiego2012@gmail.com>
2026-04-09 07:27:45 -0500

    Update [link].js

--- api/[link].js
+++ api/[link].js
@@ -1,14 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+
 export default function handler(req, res) {
-  const links = {
-    link1: "https://sites.google.com/view/math-staar-review/home"
-  };
+  const filePath = path.resolve('./links.json');
+  const data = JSON.parse(fs.readFileSync(filePath));
 
   const { link } = req.query;
 
-  if (links[link]) {
-    res.writeHead(302, { Location: links[link] });
-    res.end();
-  } else {
-    res.status(404).send("Not found");
+  if (!data[link]) {
+    return res.status(404).send("Not found");
   }
+
+  const linksArray = data[link];
+
+  // rotate based on time (every 3 days)
+  const index = Math.floor(Date.now() / (1000 * 60 * 60 * 24 * 3)) % linksArray.length;
+
+  const target = linksArray[index];
+
+  res.writeHead(302, { Location: target });
+  res.end();
 }
